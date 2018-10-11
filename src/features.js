3af4985e534d5ac17cc7dabd29d8bde96d93b68d
AJ Mcgladdery and Kendra Kelly <aj@>
2018-10-11 13:03:33 -0700

    add in individual color markers and get a modal to pop upon clicking

---
+++ src/features.js
@@ -0,0 +1,79 @@
+
+
+export var features = [
+  {
+    position: new window.google.maps.LatLng(45.523073, -122.641668),
+    title: 'Screen Door',
+    type: 'restaurant',
+    content: '<div><h5>Screen Door</h5>'+
+              '<p>Refined Southern specialties like buttermilk-fried chicken in roomy digs offering weekend brunch.</p>'+
+              '<p>2337 E Burnside St, Portland, OR 97214</p>'+
+              '<p>(503) 542-0880</p>'+
+              '<a href="https://www.google.com/maps/place/Screen+Door/@45.5229893,-122.6438357,17z/data=!4m5!3m4!1s0x5495a0beac1053bb:0x8606af39a198e136!8m2!3d45.5229894!4d-122.6416469">View on Google Maps</a>'+
+              '</div>'
+  }, {
+    position: new window.google.maps.LatLng(45.520835, -122.683431),
+    title: 'Blue Star Donuts',
+    type: 'restaurant'
+  }, {
+    position: new window.google.maps.LatLng(45.590319, -122.755167),
+    title: 'Tienda Santa Cruz',
+    type: 'restaurant'
+  }, {
+    position: new window.google.maps.LatLng(45.540782,  -122.663615),
+    title: 'Toro Bravo',
+    type: 'restaurant'
+  }, {
+    position: new window.google.maps.LatLng(45.523519, -122.681595),
+    title: 'Powells City of Books',
+    type: 'indoor'
+  }, {
+    position: new window.google.maps.LatLng(45.473750, -122.660508),
+    title: 'Roller Derby',
+    type: 'indoor'
+  }, {
+    position: new window.google.maps.LatLng(45.535436, -122.620610),
+    title: 'Hollywood Theater',
+    type: 'indoor'
+  }, {
+    position: new window.google.maps.LatLng(45.524302, -122.675914),
+    title: 'Ground Kontrol',
+    type: 'indoor'
+  }, {
+    position: new window.google.maps.LatLng(45.512490, -122.656361),
+    title: 'Lucky Lab',
+    type: 'brewery'
+  }, {
+    position: new window.google.maps.LatLng(45.504613, -122.632161),
+    title: 'Pok Pok',
+    type: 'restaurant'
+  }, {
+    position: new window.google.maps.LatLng(45.511635, -122.594540),
+    title: 'Mount Tabor Park',
+    type: 'outdoor'
+  }, {
+    position: new window.google.maps.LatLng(-33.91662347903106, 151.22879464019775),
+    type: 'parking'
+  }, {
+    position: new window.google.maps.LatLng(-33.916365282092855, 151.22937399734496),
+    type: 'parking'
+  }, {
+    position: new window.google.maps.LatLng(-33.91665018901448, 151.2282474695587),
+    type: 'parking'
+  }, {
+    position: new window.google.maps.LatLng(-33.919543720969806, 151.23112279762267),
+    type: 'parking'
+  }, {
+    position: new window.google.maps.LatLng(-33.91608037421864, 151.23288232673644),
+    type: 'parking'
+  }, {
+    position: new window.google.maps.LatLng(-33.91851096391805, 151.2344058214569),
+    type: 'parking'
+  }, {
+    position: new window.google.maps.LatLng(-33.91818154739766, 151.2346203981781),
+    type: 'parking'
+  }, {
+    position: new window.google.maps.LatLng(-33.91727341958453, 151.23348314155578),
+    type: 'library'
+  }
+];
